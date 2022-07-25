6bf8088815c824f5123d8691bab330599675afcf
Jose Nortes <nooors@gmail.com>
2022-07-25 08:18:58 +0200

    express server default settings

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("hey");
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import { createServer } from "http";
+
+const app = express();
+
+app.use("*", cors);
+app.use(compression());
+
+app.get("/", (req, res) => {
+  res.send("Hello to online academy with GraphQl");
+});
+
+const httpServer = createServer(app);
+
+const PORT = 5200;
+
+httpServer.listen(
+  {
+    port: PORT,
+  },
+  () => console.log(`Acdemy online server runing on http://localhost:${PORT}`),
+);
