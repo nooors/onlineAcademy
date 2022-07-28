692b2ad48fbb906f4909094ab9f607038898c877
Jose Nortes <nooors@gmail.com>
2022-07-28 17:24:20 +0200

    nothing changes

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,7 @@
-import { GraphQLSchema } from "graphql";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import "graphql-import-node";
 import typeDefs from "./schema.graphql";
+import { GraphQLSchema } from "graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import resolvers from "../resolvers/resolversMap";
 
 const schema: GraphQLSchema = makeExecutableSchema({
